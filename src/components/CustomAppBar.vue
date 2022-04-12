a192f7d48f859dd6e53962234a9dff4ad4d5cbb3
micmanan <michaeljohn.mananghaya@finastra.com>
2022-04-12 13:31:08 +0800

    v0.1-alpha_20220412-login
    * Added vee-validate dependency.
    * Created a login form complete with validations for empty and blank strings.
    * Created a log out button that will be shown once the user has been authenticated.
    * Created a menu located on the far right of the app bar. It houses the log in form and the log out button.

--- src/components/CustomAppBar.vue
+++ src/components/CustomAppBar.vue
@@ -1,18 +1,39 @@
 <template>
   <v-app-bar
     app
-    dense
     flat
+    dense
     clipped-left
   >
-
+    <v-toolbar-title>Application Name</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-menu
+      transition="scroll-y-transition"
+      :close-on-content-click="false"
+      :nudge-bottom="8"
+      :nudge-width="200"
+      offset-y
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          icon
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+      <login-form />
+    </v-menu>
   </v-app-bar>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import LoginForm from './LoginForm.vue'
 
 export default Vue.extend({
+  components: { LoginForm },
   name: 'CustomAppBar',
 
   data: () => ({})
