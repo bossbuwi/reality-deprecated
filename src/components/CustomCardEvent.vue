f5f435c3d0cd37f1c3fda50daf801b692560d3b1
micmanan <mjmananghaya@gmail.com>
2022-04-13 14:26:39 +0800

    v0.1-alpha_20220413-dashboard
    Dashboard UI done, although this is just barebones and without any logic under the hood.
    
    * Added new custom cards, one for the latest event and another for a generic information card. These would be used by the Dashboard and could also be reused by other views in the future.

---
+++ src/components/CustomCardEvent.vue
@@ -0,0 +1,150 @@
+<template>
+    <v-card
+      hover
+    >
+      <v-card-title>
+        <v-row>
+          <v-col
+            cols="1"
+          >
+            <v-icon
+              large
+              color="blue darken-2"
+            >
+             mdi-calendar-text-outline
+            </v-icon>
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col
+            cols="auto"
+          >
+            Latest Event
+          </v-col>
+        </v-row>
+      </v-card-title>
+
+      <v-card-text
+        class="text-body-1"
+      >
+        <v-row>
+          <v-col
+            cols="auto"
+          >
+            System
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col
+            cols="auto"
+          >
+            {{ globalPrefix }}
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col
+            cols="auto"
+          >
+            Zones
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col
+            cols="auto"
+          >
+            {{ zones }}
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col
+            cols="auto"
+          >
+            Event Types
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col
+            cols="auto"
+          >
+            {{ eventTypes }}
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col
+            cols="auto"
+          >
+            Start Date
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col
+            cols="auto"
+          >
+            {{ startDate }}
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col
+            cols="auto"
+          >
+            End Date
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col
+            cols="auto"
+          >
+            {{ endDate }}
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col
+            cols="auto"
+          >
+            Last Modified By
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col
+            cols="auto"
+          >
+            {{ lastModifiedBy }}
+          </v-col>
+        </v-row>
+      </v-card-text>
+
+      <v-divider
+        class="mx-8"
+      >
+      </v-divider>
+
+      <v-card-actions>
+        <v-btn text class="text-capitalize">
+          <v-icon right>
+             mdi-refresh
+          </v-icon>
+          &nbsp;Refresh
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'CustomCardEvent',
+
+  props: [
+    'globalPrefix', 'zones', 'eventTypes', 'startDate', 'endDate', 'lastModifiedBy'
+  ],
+
+  data () {
+    return {
+
+    }
+  },
+
+  methods: {
+
+  }
+})
+</script>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,15 +1,105 @@
 <template>
-  <div class="dashboard">
-    <h1>This is the dashboard page</h1>
-  </div>
+  <v-container fluid>
+    <v-row>
+      <v-col
+        cols="12"
+      >
+        <custom-card-event
+          :globalPrefix="event.globalPrefix"
+          :zones="event.zones"
+          :eventTypes="event.eventTypes"
+          :startDate="event.startDate"
+          :endDate="event.endDate"
+          :lastModifiedBy="event.lastModifiedBy"
+        >
+        </custom-card-event>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col
+        cols="4"
+      >
+        <custom-card-basic
+          :cardIcon="systems.icon"
+          :cardName="systems.name"
+          :cardContent="systems.content"
+        >
+        </custom-card-basic>
+      </v-col>
+
+      <v-col
+        cols="4"
+      >
+        <custom-card-basic
+          :cardIcon="events.icon"
+          :cardName="events.name"
+          :cardContent="events.content"
+        >
+        </custom-card-basic>
+      </v-col>
+
+      <v-col
+        cols="4"
+      >
+        <custom-card-basic
+          :cardIcon="users.icon"
+          :cardName="users.name"
+          :cardContent="users.content"
+        >
+        </custom-card-basic>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import CustomCardBasic from '@/components/CustomCardBasic.vue'
+import CustomCardEvent from '@/components/CustomCardEvent.vue'
 
 export default Vue.extend({
   name: 'DashboardView',
 
-  components: {}
+  components: {
+    CustomCardBasic,
+    CustomCardEvent
+
+  },
+
+  data () {
+    return {
+      systems: {
+        name: 'Systems',
+        icon: 'mdi-web',
+        content: '2'
+      },
+
+      events: {
+        name: 'Events',
+        icon: 'mdi-calendar',
+        content: '20'
+      },
+
+      users: {
+        name: 'Users',
+        icon: 'mdi-heart-outline',
+        content: '5'
+      },
+
+      event: {
+        globalPrefix: 'OS',
+        zones: 'O4, O5',
+        eventTypes: 'SYSMAINT',
+        startDate: '2022-04-12',
+        endDate: '2022-04-15',
+        lastModifiedBy: 'admin'
+      }
+    }
+  },
+
+  methods: {
+
+  }
 })
 </script>
