8f82b74ce50a0a81df0982c0d3c8a5c045a3f244
micmanan <michaeljohn.mananghaya@finastra.com>
2022-04-25 10:19:37 +0800

    v0.1-alpha_20220425-system
    * Added a primitive New System form, not unlike the New Event form. However, this is inadequate because a system has so many dependencies. I might need to create a separate page just for system creation, together with all of its dependencies. Committing this just in case it would be needed in the future.

--- src/views/SystemsView.vue
+++ src/views/SystemsView.vue
@@ -8,6 +8,7 @@
         <v-spacer></v-spacer>
         <v-btn
           color="primary"
+          @click.stop="dialog = true"
         >
           <v-icon left>
             mdi-playlist-plus
@@ -25,6 +26,112 @@
         </v-data-table>
       </v-card-text>
     </v-card>
+    <!-- Dialog -->
+    <v-dialog
+      v-model="dialog"
+      persistent
+      eager
+      fullscreen
+    >
+      <v-card>
+        <v-toolbar
+          dark
+          dense
+          color="primary"
+        >
+          <v-btn
+            icon
+            dark
+            @click="dialog = false"
+          >
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>New System</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-select
+                  :items="['MNQ', 'MNP']"
+                  label="Machine*"
+                  required
+                ></v-select>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="System Prefix*"
+                  hint="Must be unique within the selected machine."
+                  type="text"
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Software Release*"
+                  type="text"
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="6"
+              >
+                <v-text-field
+                  label="URL"
+                  type="text"
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="6"
+              >
+                <v-text-field
+                  label="System Admin(s)"
+                  hint="Please separate entries with a comma."
+                  type="text"
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="6"
+              >
+                <v-text-field
+                  label="User Login"
+                  hint="Please separate entries with a comma."
+                  type="text"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Submit
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -38,6 +145,7 @@ export default Vue.extend({
 
   data () {
     return {
+      dialog: false,
       headers: [
         {
           text: 'System Prefix',
