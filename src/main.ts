d70bcf36ca5a47d88e94d9ddf9686bc758283f90
micmanan <michaeljohn.mananghaya@finastra.com>
2022-04-27 15:15:34 +0800

    v0.1-alpha_20220427-login-logic
    * Added axios and vuex-persistedstate dependencies to handle server connection and session persistence respectively.
    * Changed development port from 8080 to 8090 to prevent clashing with the development backend. This change results in a proxy workaround below.
    * Added a development proxy that prevents vuejs from discarding outgoing HTTP request because of CORS. This should be reevaluated once production build is ready. It should not be deployed in production.
    * Added a new module on vuejs' store, the auth module. This handles the user's state and authentication.
    * Modified Login.vue, removed the hard coded parts (form loading, logout, etc) and replaced them with legit server side communications using axios and vuex. Errors are not yet handled though.

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:8090/'
 
 new Vue({
   router,
